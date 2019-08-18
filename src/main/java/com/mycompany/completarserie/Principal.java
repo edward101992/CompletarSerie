a500a10af51e02b8a6d11466ded21f7b00c36f91
Edward <edwardconw@gmail.com>
2019-08-18 12:43:27 -0500

    Metodo llenar Vector Metodo mayor y menor

--- src/main/java/com/mycompany/completarserie/Principal.java
+++ src/main/java/com/mycompany/completarserie/Principal.java
@@ -14,11 +14,43 @@ public class Principal {
     
      Scanner sc = new Scanner(System.in);
   
+     
      public void pedirVector(){
          int limite=0;
          System.out.println("Cuantos numeros va a ingresar:  ");
          limite = sc.nextInt();
+         llenarVector(limite);
          
      }
-    
+     
+        
+     public void llenarVector(int limite){
+         int vector[]= new int[limite];
+         int i;
+         for(i=0;i<limite;i++){
+             System.out.println("Ingrese el numero  " + (i+1) + " :  ");
+             vector[i]=sc.nextInt();
+         }
+         mayorMenor(vector,limite);
+     }
+     
+      public void mayorMenor(int vector[],int limite){
+         int mayor = 0;
+         int menor = 0;
+         int fin = limite;
+         int array[]=vector;
+         mayor = menor = vector[0];       
+         for(int i=0;i<limite;i++){
+             if(vector[i]>mayor){
+                 mayor=vector[i];
+             }
+             if(vector[i]<menor){
+                 menor=vector[i];
+             }
+         }
+     }
+     
+     
+         
+         
 }

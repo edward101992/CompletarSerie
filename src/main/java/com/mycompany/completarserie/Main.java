4442141ad31c5fad61099d643e1567f6b9a4640e
Edward <edwardconw@gmail.com>
2019-08-18 12:40:22 -0500

    Metodo Pedir vector

--- src/main/java/com/mycompany/completarserie/Main.java
+++ src/main/java/com/mycompany/completarserie/Main.java
@@ -1,13 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Clase main donde Inicia el programa
  */
 package com.mycompany.completarserie;
 
 /**
  *
- * @author SuperUs
+ * @author Edward Ramos
+ * @author Victor Preciado
  */
 public class Main {
 
--- src/main/java/com/mycompany/completarserie/Principal.java
+++ src/main/java/com/mycompany/completarserie/Principal.java
@@ -1,14 +1,24 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Clase Principal la cual contiene la parte logica del programa
  */
 package com.mycompany.completarserie;
 
+import java.util.Scanner;
+
 /**
  *
- * @author SuperUs
+ * @author Edward Ramos
+ * @author Victor Preciado
  */
 public class Principal {
+    
+     Scanner sc = new Scanner(System.in);
+  
+     public void pedirVector(){
+         int limite=0;
+         System.out.println("Cuantos numeros va a ingresar:  ");
+         limite = sc.nextInt();
+         
+     }
     
 }
